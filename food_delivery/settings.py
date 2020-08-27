b25bdddbb8d00f93fc344460efccae19880ee0ab
Prince <amarjot.singh070@gmail.com>
2020-08-27 20:33:50 +0530

    Added Heroku Config

--- food_delivery/settings.py
+++ food_delivery/settings.py
@@ -120,10 +120,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 LOGOUT_REDIRECT_URL = '/restaurant/sign-in/'
 LOGIN_REDIRECT_URL = '/restaurant/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
+
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
