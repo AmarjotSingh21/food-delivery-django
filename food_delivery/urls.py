5de61500dbe6359499277f900f705cf1b645c417
Prince <amarjot.singh070@gmail.com>
2020-08-27 13:49:00 +0530

    Added signup view

--- food_delivery/urls.py
+++ food_delivery/urls.py
@@ -4,9 +4,5 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('restaurant/sign-in/', auth_views.LoginView.as_view(
-        template_name='restaurant/sign_in.html'), name='restaurant-sign-in'),
-    path('restaurant/sign-out/', auth_views.LogoutView.as_view(
-        template_name='restaurant/sign_out.html',), name='restaurant-sign-out'),
     path('restaurant/', include('restaurant.urls')),
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+
 @login_required(login_url='/restaurant/sign-in/')
 def restaurant_home(request):
     return render(request, 'restaurant/home.html')
+
+
+def restaurant_sign_up(request):
+    return render(request, 'restaurant/sign_up.html')
